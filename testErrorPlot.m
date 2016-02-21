6d87f1c83808b79c564f035047c1ea2eade45d69
sea dubbs <christian.radcliffe.ward@gmail.com>
2016-02-21 16:24:01 -0500

    helper functions. better late than never.

---
+++ testErrorPlot.m
@@ -0,0 +1,37 @@
+function testErrorPlot(test_error)
+
+% row are number of tests, nMixtures
+% column are number of subjects
+[row,column] = size(test_error);
+
+% generate proper data labels for subjects
+legend_labels = cell(column,1);
+for i=1:column
+    legend_labels{i} = ['Subject-',num2str(i)];
+end
+
+% generate x axis tick labels
+x_labels = cell(row,1);
+for i=1:row
+    x_labels{i} = num2str(2^i);
+end
+
+line_flash = {'--+', ':o', '--*', ':.', '--s', ':x'};
+
+figure('numbertitle','off','name','UBM EER Result');
+ax = gca;
+hold on;
+grid on;
+
+for i=1:column
+    plot(test_error(:,i),line_flash{i},'linewidth',2,'MarkerSize',14);
+end
+legend(legend_labels,'location','eastoutside');
+ylabel('EER %','FontSize',14);
+xlabel('Gaussian Mixtures','FontSize',14);
+title('Subect Train Set: EER versus Gaussian Mixture Size','FontSize',14)
+set(ax,'XTick',[1:row]);
+set(ax,'XTickLabel',x_labels);
+set(ax,'FontSize',14);
+
+end
