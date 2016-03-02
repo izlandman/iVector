9de6aa04d5592fca09c61139964930fc12fc0a3d
sea dubbs <christian.radcliffe.ward@gmail.com>
2016-03-02 14:39:53 -0500

    attempts to make cleaner file saving folder names

--- splitFeaturesFolders.m
+++ splitFeaturesFolders.m
@@ -8,20 +8,27 @@ cluster = parcluster('local');
 cluster.NumWorkers = workers;
 parpool(workers);
 
-% pulls all .mat files, so the directories need to be clean
-file_list = findMatchingFiles(root_directory,'.mat');
+disp(['Starting directory: ',pwd]);
 
+cd(root_directory);
+
+disp(['Changed directory to: ',pwd]);
+
+% pulls all .mat files, so the directories need to be clean
+file_list = findMatchingFiles('.','.mat');
+disp(['First file: ',file_list{1}]);
+disp(['Last file: ',file_list{end}]);
 % assuming the file_list is only valid .mat files generated as feature sets
 
 file_count = numel(file_list);
-
+disp(file_count);
 parfor i=1:file_count
     % be careful since every feature set is labeled 'set_c'
     a(i) = load(file_list{i});
     [b{i},c{i},~] = fileparts(file_list{i});
-    
+    d{i} = strsplit(b{i},'.');
     try
-        melTestTrainSplit_2(a(i).set_c,split,b{i},c{i});
+        melTestTrainSplit_2(a(i).set_c,split,['/_split',d{i}(end)],c{i});
     catch
         disp(['Train/Test Split Failed: ',file_list{i}]);
     end
